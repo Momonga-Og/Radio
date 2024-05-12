2019bf466724573b3744cef840baab5eb4edf0df
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-05-12 13:48:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import interactions
 logging.basicConfig(level=logging.INFO)
 
 # Use interactions.Intents for defining intents
-intents = interactions.Intents()
+intents = interactions.Intents(value=0)  # You can adjust the value as needed
 intents.voice_states = True
 intents.guilds = True
 
